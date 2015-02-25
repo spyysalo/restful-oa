5f5f9e7513b32f2e9e7aa1cee97e682875245e56
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2015-02-25 11:30:02 +0900

    minimal test

---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+BASE='http://127.0.0.1:5000/annotations'
+
+curl -X PUT -i \
+    -H 'Content-Type: application/json' \
+    -d '{'\
+'"@id": 1, '\
+'"hasBody": "http://example.org/foo",'\
+'"hasTarget": "http://example.org/doc.txt#char=0,10"'\
+'}' \
+    "$BASE/1"
+
+curl -X GET -i -H 'Accept: application/json' "$BASE/1"
