dd297b9327eef869a6cee56cdb739019cb0cb4f1
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2015-02-25 09:40:03 +0900

    minimal in-memory storage

---
+++ memstore.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+from store import Store
+
+class MemStore(Store):
+    """In-memory id-based storage."""
+    
+    def __init__(self, id_key=None):
+        super(MemStore, self).__init__(id_key)
+        self.obj_by_id = {}
+        self._next_id = 1
+
+    def get(self, id_):
+        return self.obj_by_id[id_]
+
+    def put(self, obj, id_=None):
+        if id_ is None:
+            id_ = self.obj_id(obj)
+        self.obj_by_id[id_] = obj
+
+    def insert(self, obj):
+        try:
+            id_ = self.obj_id(obj)
+        except KeyError:
+            id_ = self._assign_id(obj)
+        self.put(obj, id_)
+
+    def delete(self, id_):
+        del self.obj_by_id[id_]
+
+    def ids(self):
+        return self.obj_by_id.keys()
+
+    def _assign_id(self, obj):
+        while True:
+            if self._next_id not in self.obj_by_id:
+                break
+            self._next_id += 1
+        obj[self._id_key] = self._next_id
+        self._next_id += 1
+        return obj[self._id_key]
--- store.py
+++ store.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python
 
+DEFAULT_ID_KEY='@id'
+
 class Store(object):
     """Abstract base for id-based storage."""
     
-    def __init__(self):
-        raise NotImplementedError
+    def __init__(self, id_key=None):
+        if id_key is None:
+            id_key = DEFAULT_ID_KEY
+        self._id_key = id_key
+
+    def obj_id(self, obj):
+        return obj[self._id_key]
 
     def get(self, id_):
         raise NotImplementedError
 
-    def put(self, obj):
+    def put(self, obj, id_=None):
         raise NotImplementedError
 
     def insert(self, obj):
