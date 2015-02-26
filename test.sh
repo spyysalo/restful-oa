4ae3e4cfabe341e77d37e7555d628ec5253bccf6
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2015-02-26 14:34:34 +0900

    don't add dummy example

--- test.sh
+++ test.sh
@@ -4,14 +4,14 @@ ROOT="http://127.0.0.1:5000"
 BASE="$ROOT/annotations"
 DOCS="$ROOT/documents"
 
-curl -X PUT -i \
-    -H 'Content-Type: application/json' \
-    -d '{'\
-'"@id": "1", '\
-'"hasBody": "http://example.org/foo",'\
-'"hasTarget": "http://example.org/doc.txt#char=0,10"'\
-'}' \
-    "$BASE/1"
+# curl -X PUT -i \
+#     -H 'Content-Type: application/json' \
+#     -d '{'\
+# '"@id": "1", '\
+# '"hasBody": "http://example.org/foo",'\
+# '"hasTarget": "http://example.org/doc.txt#char=0,10"'\
+# '}' \
+#     "$BASE/1"
 
 curl -X GET "$DOCS/test.txt"
 
